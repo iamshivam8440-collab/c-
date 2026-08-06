ca6cea374672ed1d454504d2ba578b45bdc6f309
Shivam Chaurasiya <iamshivam8440@gamil.com>
2026-08-06 22:30:31 +0530

    first commit

---
+++ endl.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello";
+	cout<<"World";
+	cout<<"----------------------"<<endl;
+	cout<<"Hello"<<endl;
+	cout<<"World"<<endl;
+	cout<<"----------------------"<<endl;
+	cout<<"Hello\n";
+	cout<<"World\n";
+	return 0;
+}
