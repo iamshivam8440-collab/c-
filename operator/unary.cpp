df4d72ba5cd5a7d22e59ebb59a8617e21cabe246
Shivam Chaurasiya <iamshivam8440@gamil.com>
2026-08-06 18:31:04 +0530

    second commit

--- operator/unary.cpp
+++ operator/unary.cpp
@@ -8,13 +8,20 @@ int main()
 	cin>>a;
 	/* pre increment  first increment then print*/
 	cout<<"Pre Increment of is "<<++a<<endl;
+
+
 	/* post increment first print then increment */
 	cout<<"Post Increment of is "<<a++<<endl;
+
+
 	int b;
 	cout<<"Enter the number:";
 	cin>>b;
+
 	/* pre decrement  first decrement then print*/
 	cout<<"Pre Decrement of is "<<--b<<endl;
+
+
 	/* post decrement first print then decrement */
 	cout<<"Post Decrement of is "<<b--<<endl;
 	return 0;
