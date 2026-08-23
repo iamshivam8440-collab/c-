d085bae3f715a60e5d3b874cb7db5ed8682cbd43
Shivam Chaurasiya <iamshivam8440@gamil.com>
2026-08-23 22:07:32 +0530

    first commit

--- signal.cpp
+++ signal.cpp
@@ -1,25 +1,42 @@
 #include<iostream>
 using namespace std;
 void signal(char );
-void signal(char light)
+void signal(int light)
 {
 
-	if()
-	cout<<"Stop"<<endl;
-	else if(light)
-	cout<<"Go"<<endl;
+	if(light==1)
+	{
+		cout<<"----------------------\n";
+		cout<<"Signal is Green-Go!!"<<endl;
+		cout<<"----------------------\n";
+	}
+	else if(light==2)
+	{
+		cout<<"----------------------\n";
+		cout<<"Signal is Red-Stop!!"<<endl;
+		cout<<"----------------------\n";
+	}
 	else
-	cout<<"Go slow"<<endl;
+	{
+		cout<<"----------------------\n";
+		cout<<"Signal is Yellow-Wait!!"<<endl;
+		cout<<"----------------------\n";
+	}
 }
 int main()
 {
-	char light[10];
+	int light;
 	cout<<"Signal light color:"<<endl;
 	cout<<"--------------------"<<endl;
 	cout<<"1.Green"<<endl<<"2.Red"<<endl<<"3.Yellow"<<endl;
 	cout<<"--------------------"<<endl;
+	cout<<"*************************************\n";
+	cout<<"Enter number for signal light color:"<<endl;
+	cout<<"1 for Green"<<endl<<"2 for Red"<<endl<<"3 for Yellow"<<endl;
+	cout<<"*************************************\n";
 	cout<<"Enter signal light color:";
 	cin>>light;
 	signal(light);
 	return 0;
 }
+
