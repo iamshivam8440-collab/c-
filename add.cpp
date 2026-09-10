0843e2525b372f4573c737a4ef09cb7cb23f592f
Shivam Chaurasiya <iamshivam8440@gamil.com>
2026-09-10 23:23:41 +0530

    first commit

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int a,b;
+    std::cout<<"Enter the value a:"<<std::endl;
+    std::cin>>a;
+    std::cout<<"Enter the value b:"<<std::endl;
+    std::cin>>b;
+    std::cout<<"Addition of a and b is:"<<a+b<<std::endl;
+    return 0;
+}
